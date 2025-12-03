4abd77ec87faedaa97de4a95202a5cccef212c8f
SaikYouBoi <iliaselggames@gmail.com>
2025-12-03 09:10:36 +0100

    compatible db with csv file

--- BLOGCMS.sql
+++ BLOGCMS.sql
@@ -44,3 +44,49 @@ CREATE TABLE COMMENTAIRE(
     FOREIGN KEY (id_user) REFERENCES USER(id_user),
     FOREIGN KEY (id_article) REFERENCES ARTICLE(id_article)
 );
+
+
+--COMPATIBLE DATA WITH THE CSV FILE
+
+CREATE TABLE USER(
+	id_user INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NO NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    inscription_date DATE
+)
+
+CREATE TABLE CATEGORY(
+	id_category INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+)
+
+CREATE TABLE POST(
+	id_post INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    content TEXT),
+    creation_date DATE NOT NULL,
+    update_date DATE,
+    status VARCHAR(50) NOT NULL,
+    view_count VARCHAR(50) NOT NULL,
+    id_user INT NOT NULL,
+    id_category INT NOT NULL,
+    
+    FOREIGN KEY (id_user) REFERENCES USER(id_user),
+    FOREIGN KEY (id_category) REFERENCES CATEGORY(id_category)
+);
+
+
+CREATE TABLE COMMENTS(
+	id_comment INT PRIMARY KEY AUTO_INCREMENT,
+    contenu_commentaire VARCHAR(50),
+    creation_date DATE NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    id_user INT NOT NULL,
+    id_post INT NOT NULL,
+    
+    FOREIGN KEY (id_user) REFERENCES USER(id_user),
+    FOREIGN KEY (id_article) REFERENCES POST(id_post)
+);
